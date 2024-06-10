dd9bf1eadf06a95f8da0ee105c1fc5d8400121a7
Tim Goshinski <tim@code-chimp.com>
2024-06-09 22:49:44 -0500

    :sparkles: Add a color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electric-violet': {
+          50: '#f6f2ff',
+          100: '#ede8ff',
+          200: '#ddd4ff',
+          300: '#c6b1ff',
+          400: '#ab85ff',
+          500: '#843eff',
+          600: '#8430f7',
+          700: '#761ee3',
+          800: '#6318bf',
+          900: '#52169c',
+          950: '#320b6a',
+        },
+      },
+    },
   },
   plugins: ['@tailwindcss/forms'],
 };
